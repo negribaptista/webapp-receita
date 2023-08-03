cbd0ae63f57d1160a3dff38d42cc3b946d12294a
negribaptista <emanuellemariavieira@gmail.com>
2023-08-03 14:50:56 -0300

    index adicionado

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include 'cabeçalho.php'; ?>
+<body>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-8">
+
+      <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+     </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+      </div>
+    </div>
+  </div>
+  <?php 
+  include 'rodape.php';?>
